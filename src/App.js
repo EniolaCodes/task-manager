2cbb19e5e37ec076764458af690155028c6cc83a
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2024-02-28 13:53:00 +0100

    create components

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Header from './components/Header';
+// import Button from './components/Button';
+import TaskList from './components/TaskList';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Header/>
+    {/* <Button/> */}
+    <TaskList/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/Button/index.jsx
@@ -0,0 +1,18 @@
+
+
+// const Button = () =>{
+//     const [showTaskEdit, setShowTaskEdit] = useState(false);
+
+//     <button
+//       className="button outline"
+//       onClick={() => setShowTaskEdit(!showTaskEdit)}>
+//       {!showTaskEdit && "New"}
+//       {showTaskEdit && "➖"}
+//     </button>;
+// }
+// export default Button;
+
+
+
+
+
